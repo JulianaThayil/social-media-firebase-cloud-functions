e424747c4956efa5c99177291fcb8a830a730e96
juliana thayil <julianathayil@gmail.com>
2020-03-18 20:00:49 +0530

    initial commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyC8HRiOo1srYT0DveHZf1xxj6biEX6R0vI",
+    authDomain: "step-by-step-89b62.firebaseapp.com",
+    databaseURL: "https://step-by-step-89b62.firebaseio.com",
+    projectId: "step-by-step-89b62",
+    storageBucket: "step-by-step-89b62.appspot.com",
+    messagingSenderId: "628056435424",
+    appId: "1:628056435424:web:e5e14db32c5aadd3c97e85",
+    measurementId: "G-GZB8JZRBNT"
+  };
