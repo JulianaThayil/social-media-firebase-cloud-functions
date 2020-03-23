9bd3d3cda65304370dca5a0f3c89a7a870759d8e
Juliana Thayil <47752094+JulianaThayil@users.noreply.github.com>
2020-03-23 13:10:33 +0530

    Update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
-module.exports = {
-  apiKey: "AIzaSyDZDYNoTcSesSR6a1Vyf25P_oTzZKaTO90",
-  authDomain: "step-by-step-96e75.firebaseapp.com",
-  databaseURL: "https://step-by-step-96e75.firebaseio.com",
-  projectId: "step-by-step-96e75",
-  storageBucket: "step-by-step-96e75.appspot.com",
-  messagingSenderId: "593075018975",
-  appId: "1:593075018975:web:4d6a21fdbaf1ca99cf5d6a",
-  measurementId: "G-KQQTR83J4N"
-  };
-=======
 
->>>>>>> 80d5b8734cd74e1b294c797da2b84c2f22de4552
